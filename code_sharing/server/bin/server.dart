fc6f6ade0d70e029a12b0b7a0616c1683ed040b5
v_liyu6 <v_liyu6@tal.com>
2025-12-29 19:08:50 +0800

    feat:coding_shared 完成

---
+++ code_sharing/server/bin/server.dart
@@ -0,0 +1,42 @@
+
+
+// Configure routes.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:shared/shared.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+int count =0;
+
+final _router = Router()
+ ..post('/', _incrementHandler)
+ ..get('/', _getValueHandler);
+
+Future<Response> _incrementHandler(Request request) async {
+  final incr = Increment.fromJson(json.decode(await request.readAsString()));
+  count += incr.by;
+  return Response.ok(json.encode(Count(count).toJson()));
+}
+
+Response _getValueHandler(Request request) =>
+    Response.ok(json.encode(Count(count).toJson()));
+
+Future<void> main(List<String> arguments) async {
+  // Use any available host or container IP (usually `0.0.0.0`).
+  final ip = InternetAddress.anyIPv4;
+
+
+  // Configure a pipeline that logs requests.
+  final handler = Pipeline()
+  .addMiddleware(logRequests())
+  .addHandler(_router.call);
+
+// For running in containers, we respect the PORT environment variable.
+final port = int.parse(Platform.environment['PORT'] ?? '9090');
+final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
